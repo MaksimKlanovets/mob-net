427dd7cf604dd39382fd9f13588e917faf4ba6af
MaksimKlanovets <klanovetsm@gmail.com>
2021-06-12 18:46:28 +0300

    some changes

--- major/headers/NetConfAgent.hpp
+++ major/headers/NetConfAgent.hpp
@@ -3,32 +3,44 @@
 using namespace std;
 //data for test function NetConfAgent::print_current_config
 #define MAX_LEN 100
+
+
+//tasks 1. unown namespace in .cpp for helper function and const, difane
+//futch data with mape path and value
+// change point to refer
+// _subcriber move to private,  private fild start with _
+//registerOperdata move xpath to first arg in map
+//think about implementation thread
+
 namespace ns_NetConf
 {
 
 class NetConfAgent
 {
-private:
-    shared_ptr<sysrepo::Session> m_Session;
-    shared_ptr<sysrepo::Connection> m_Connection;
 public:
     NetConfAgent();
     ~NetConfAgent();
-
+/*
+*@breaf Fethch data....
+*
+*@param name Module name.
+*
+*@return true if ok, otherwise false
+*/
     //initialization sysrepo
     bool initSysrepo( );
     //write data from path
-    bool fetchData(const char *xpath, string &data,const string &key);
+    bool fetchData(pair<string,string>&pairFromFetch);
     //subcriber for model Changes 
-    bool subscriberForModelChanges(const string *module_name);
+    bool subscriberForModelChanges(const string &module_name);
     // set noconfig data, call back for them 
-    bool registerOperData( const string *module_name,const string *xpath, map<string,string>*userName);
+    bool registerOperData( const string &module_name,const pair<string,string> &setData);
 
-    bool subscriberForRpc(const string *module_name);
+    bool subscriberForRpc(const string &module_name);
     //sent some notif 
-    bool notifySysrepo(const string *module_name);
+    bool notifySysrepo(const string &module_name);
     //change data for a given path
-    bool changeData(const char *xpath,const string &value);
+    bool changeData(const pair<string,string> &setData);
 
       
     /* Function to print current configuration state.
@@ -42,6 +54,12 @@ void print_node(libyang::S_Data_Node &node);
 const char * nodetype2str(LYS_NODE type);
 static
 void print_value(sysrepo::S_Val value);
+
+private:
+//rename first _session, add _subscriber in private field
+    shared_ptr<sysrepo::Session> _session;
+    shared_ptr<sysrepo::Subscribe> _subscribe;
+    shared_ptr<sysrepo::Connection> _connection;
 };
 
 }
